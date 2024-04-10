9e880d9a57ec385c0adff52d2981c9531416a624
mogronalol <andrewmorgan36@gmail.com>
2024-04-09 20:38:09 -0700

    Demo 2 completed

--- m2-a01-access-control-attack-vectors-and-mitigation-in-spring-applications/m2-demo-1-deny-by-default/src/main/java/pluralsight/m2/config/SecurityConfig.java
+++ m2-a01-access-control-attack-vectors-and-mitigation-in-spring-applications/m2-demo-1-deny-by-default/src/main/java/pluralsight/m2/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package pluralsight.m2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
@@ -13,22 +12,15 @@ import pluralsight.m2.repository.TestDataFactory;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-
-
         http
                 .authorizeHttpRequests(requests ->
-//                        requests
-//                                .requestMatchers("/admin/accounts").authenticated()
-//                                .requestMatchers("/**").permitAll()
-                                requests
-                                        .requestMatchers("/images/**", "/favicon.ico").permitAll()
-                                        .requestMatchers("/admin/**").authenticated()
-                                        .requestMatchers("/").authenticated()
+                        requests
+                                .requestMatchers("/images/**", "favicon.ico").permitAll()
+                                .requestMatchers("/admin/**", "/").authenticated()
                 )
                 .formLogin((form) -> form
                         .loginPage("/login")
@@ -43,6 +35,5 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService() {
         return new InMemoryUserDetailsManager(TestDataFactory.USERS);
     }
-
 }
 
