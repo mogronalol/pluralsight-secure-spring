ebf741e279018882812cdb2b8ec1e42414c95b8c
mogronalol <andrewmorgan36@gmail.com>
2024-07-15 00:22:19 -0700

    More refactoring during demos

---
+++ m14-a09-security-and-monitoring-failures/m14-demo-5-sensitive-data-masking/src/test/java/pluralsight/m13/controller/DenyByDefaultForceBrowsingTest.java
@@ -0,0 +1,64 @@
+package pluralsight.m13.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import pluralsight.m13.security.Roles;
+import pluralsight.m13.util.WithMockRole;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DenyByDefaultForceBrowsingTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void redirectToLoginPageWhenViewingAccountsWithoutLogin() throws Exception {
+
+        mockMvc.perform(get("/admin/accounts"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().stringValues("Location", "http://localhost/login"));
+    }
+
+    @Test
+    public void redirectToTransferPageWhenViewingTransfersWithoutLogin() throws Exception {
+
+        mockMvc.perform(get("/admin/transfers"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().stringValues("Location", "http://localhost/login"));
+    }
+
+    @Test
+    @WithMockRole(Roles.CUSTOMER_SERVICE)
+    public void permitViewingAccountsWhenLoggedIn() throws Exception {
+
+        mockMvc.perform(get("/admin/accounts"))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    @WithMockRole(Roles.CUSTOMER_SERVICE_MANAGER)
+    public void permitViewingTransfersWhenLoggedIn() throws Exception {
+
+        mockMvc.perform(get("/admin/transfer"))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    public void redirectToLoginPageWhenViewingAnyPageWithoutLogin() throws Exception {
+
+        mockMvc.perform(get("/" + UUID.randomUUID()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().stringValues("Location", "http://localhost/login"));
+    }
+}
---
+++ m14-a09-security-and-monitoring-failures/m14-demo-5-sensitive-data-masking/src/test/java/pluralsight/m13/util/DoubleArgumentsPerRole.java
@@ -0,0 +1,12 @@
+package pluralsight.m13.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface DoubleArgumentsPerRole {
+    double[] value() default {};
+}
